461bcdef39da02bf5b3dce6844890acf88f58b0d
AnyStepanyan <a.stepaniann@gmail.com>
2023-02-26 19:54:52 +0400

    submmit final result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+let result = true
+  let newBracketsConfig = bracketsConfig.map((val) => {
+    return `${val[0]}${val[1]}`
+  });
+
+  let initialStrLength = str.length
+
+  while (str.length) {
+    newBracketsConfig.forEach((val) => {
+      str = str.replaceAll(val, "")
+    })
+    
+    if (str.length === initialStrLength) {
+    return result = false
+    }
+  }
+  return result
 }
